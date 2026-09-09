d6698daa393f8792f7f7a184b5ac923559530a8e
GeoFrickz <george.maximillian@gmail.com>
2026-09-09 23:35:31 +0700

    fix attempt on ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
+echo "CI_WORKSPACE is: $CI_WORKSPACE"
 cd "$CI_WORKSPACE"
+echo "Now in: $(pwd)"
 echo "SPOTIFY_CLIENT_ID = $SPOTIFY_CLIENT_ID" > Secrets.xcconfig
 echo "SPOTIFY_CLIENT_SECRET = $SPOTIFY_CLIENT_SECRET" >> Secrets.xcconfig
-echo "=== Verifying ==="
 ls -la Secrets.xcconfig
-cat Secrets.xcconfig
