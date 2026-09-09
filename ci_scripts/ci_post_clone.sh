edc4327630e0ab00b95a042a52ae0e6e178c049a
GeoFrickz <george.maximillian@gmail.com>
2026-09-09 23:38:33 +0700

    more fix attempt

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
-echo "CI_WORKSPACE is: $CI_WORKSPACE"
-cd "$CI_WORKSPACE"
+echo "CI_PRIMARY_REPOSITORY_PATH is: $CI_PRIMARY_REPOSITORY_PATH"
+cd "$CI_PRIMARY_REPOSITORY_PATH"
 echo "Now in: $(pwd)"
 echo "SPOTIFY_CLIENT_ID = $SPOTIFY_CLIENT_ID" > Secrets.xcconfig
 echo "SPOTIFY_CLIENT_SECRET = $SPOTIFY_CLIENT_SECRET" >> Secrets.xcconfig
