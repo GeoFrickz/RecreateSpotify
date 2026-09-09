7c8d86ac18f98b341098a70caf98e729ff52ab18
GeoFrickz <george.maximillian@gmail.com>
2026-09-10 00:03:26 +0700

    trying automated testing

---
+++ RecreateSpotifyUITests/HomeUITests.swift
@@ -0,0 +1,21 @@
+import XCTest
+
+final class HomeUITests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        // In UI tests it's usually best to stop immediately when a failure occurs
+        continueAfterFailure = false
+    }
+    
+    func testHomeScreenLoadsTitle() throws {
+        // Launch the app
+        let app = XCUIApplication()
+        app.launch()
+        
+        // Check that the "Home" label defined in HomeViewController exists on screen
+        let homeTitleLabel = app.staticTexts["Home"]
+        
+        // Verify existence
+        XCTAssertTrue(homeTitleLabel.exists, "The main 'Home' title should be visible upon launching the app.")
+    }
+}
---
+++ RecreateSpotifyUnitTests/RecreateSpotifyUnitTests.swift
@@ -0,0 +1,18 @@
+//
+//  RecreateSpotifyUnitTests.swift
+//  RecreateSpotifyUnitTests
+//
+//  Created by George Maximillian Theodore on 09/09/26.
+//
+
+import Testing
+
+struct RecreateSpotifyUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
