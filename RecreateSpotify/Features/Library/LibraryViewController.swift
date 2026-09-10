6debd56c0023dd2a0dc3edbe43ab2b6d5ed477a0
GeoFrickz <george.maximillian@gmail.com>
2026-09-10 17:28:32 +0700

    add proper row click feedback

--- RecreateSpotify/Features/Library/LibraryViewController.swift
+++ RecreateSpotify/Features/Library/LibraryViewController.swift
@@ -92,6 +92,10 @@ class LibraryViewController: UIViewController, UITableViewDelegate, UITableViewD
         return 72
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             viewModel.deleteTrack(at: indexPath.row)
